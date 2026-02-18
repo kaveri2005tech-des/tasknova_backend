d20980f602ddc3350b47df125ec059365dde42ba
kaar-codes <webdesignkaar@gmail.com>
2026-02-18 17:31:26 +0530

    User register and login with hash user password implemented

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import express from "express";
+import { config } from "dotenv";
+config({ path: [".env"] });
+import { connect } from "mongoose";
+import server from "./app.js";
 
 const PORT = 4000;
-const server = express();
 
-server.get("/", (req, res) => {
-  res.json({ message: "Hello from my app" });
-});
+(async function () {
+  try {
+    await connect(process.env.DB_CONN);
+    console.log("DB connection succeed");
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
 
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+import isEmail from "validator/lib/isEmail.js";
+import { hash } from "bcrypt";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First name is required"],
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: [true, "Email address already exist"],
+    required: [true, "Email address is required"],
+    validate: [isEmail, "Not an Valid Email"],
+  },
+  password: {
+    type: String,
+    max: [16, "max limit of 16 characters"],
+    min: [, "min limit of 16 characters"],
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^()[\]{}])[A-Za-z\d@.#$!%*?&^()[\]{}]{8,16}$/,
+      "Password must be 8-16 characters and include uppercase, lowercase, number, and special character",
+    ],
+  },
+  confirmPassword: {
+    type: String,
+    max: [16, "max limit of 16 characters"],
+    min: [, "min limit of 16 characters"],
+    validate: [
+      function (val) {
+        return val === this.password;
+      },
+      "Confirm Password and Password didn't match",
+    ],
+  },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await hash(this.password, 8);
+  this.confirmPassword = undefined;
+});
+
+const User = model("User", userSchema);
+export default User;
