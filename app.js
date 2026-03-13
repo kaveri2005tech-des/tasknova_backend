b980d676e48bb50a9e499822fceee09445d4ed9f
kaar-codes <webdesignkaar@gmail.com>
2026-03-13 14:52:22 +0530

    Cors Updated

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import userRouter from "./routes/userRouter.js";
 import groupRouter from "./routes/groupRoute.js";
 import monoTaskRouter from "./routes/monoTaskRoute.js";
 import { createClient } from "redis";
+import cors from "cors";
 
 export const client = createClient({ port: process.env.REDIS_PORT });
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
